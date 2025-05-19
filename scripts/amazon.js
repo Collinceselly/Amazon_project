37dda1e4d36a51daac8de4f186c244f6c1e8a321
COLLINCE <omondicolli@gmail.com>
2025-05-19 19:05:36 +0300

    Add the select value of item quantity into the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -68,7 +68,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -111,13 +111,17 @@ document.querySelectorAll('.js-add-to-cart')
             }
         });
 
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        const quantityValue = Number(quantitySelector.value)
+        
+
         if (machingItem) {
-            machingItem.quantity += 1;
+            machingItem.quantity += quantityValue;
         }
         else {
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: quantityValue
             });
         }
 
@@ -131,5 +135,7 @@ document.querySelectorAll('.js-add-to-cart')
             .innerHTML = cartQuantity
 
         });
+
+        
 
     });
