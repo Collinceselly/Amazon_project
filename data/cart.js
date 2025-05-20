e4d0307bd1a4a227e55085f09411f727d11bff29
COLLINCE <omondicolli@gmail.com>
2025-05-20 13:53:31 +0300

    Save cart into local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,21 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-},
-{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')); // Retreaving the cart values as stored in the local storage
+
+
+if (!cart) { // When the site is loaded and the cart is empty, provide the default values as described by the array
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart)); // function to save the cart into local storage
+}
 
 export function addToCart(productId) {
     let machingItem;
@@ -29,6 +39,7 @@ export function addToCart(productId) {
                   quantity: quantityValue
               });
           }
+    saveToStorage() // When you add to cart save into local storage
   
   }
 
@@ -42,5 +53,7 @@ export function addToCart(productId) {
     })
 
     cart = newCart
+
+    saveToStorage(); // When you update cart save into local storage
 
   }
