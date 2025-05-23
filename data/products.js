97f86eaa6bdf759a279b10863cd4104f31161592
COLLINCE <omondicolli@gmail.com>
2025-05-23 19:31:04 +0300

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -13,6 +13,7 @@ export function getProduct(productId) {
   return marchingProduct;
 }
 
+
 class Product { 
   id;
   image;
@@ -69,6 +70,29 @@ console.log(date.toLocaleTimeString())
 console.log(date.toLocaleDateString())*/
 
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'Clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+
+    });
+    console.log('load products');
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+loadProducts();
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -735,4 +759,4 @@ export const products = [
   }
   return new Product(productDetails)
 
-});
+}); */
